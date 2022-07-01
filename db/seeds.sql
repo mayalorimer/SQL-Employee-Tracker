2558d6db0a35965010394d929b5ceae044e1af6a
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-01 16:46:38 -0700

    Initial inquirer prompts created

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,7 +13,7 @@ VALUES ("Software Engineer", 120000, 3),
     ("Salesperson", 110000, 1);
 
 
-INSERT INTO employee (first_name, last_name, role, manager_id) 
+INSERT INTO employee (first_name, last_name, role_id, manager_id) 
 VALUES ("John", "Doe", 4, null),
     ("Greg", "Smith", 1, 1),
     ("Harrison", "Pint", 2, null);
--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+// importing all the needed packages
 const express = require('express');
 const Sequelize = require('sequelize');
+const mysql = require('mysql2'); 
+const inquirer = require('inquirer');
+const console = require('console');
 
 require('dotenv').config(); 
+
+const PORT = process.env.PROT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
@@ -11,5 +21,41 @@ const sequelize = new Sequelize(
         host: 'localhost',
         dialect: 'mysql',
         port: 3306
-    }
+    },
+    console.log("Connected to the buisness database")
 );
+
+
+//function for inquirer prompts for app
+async function prompts(){
+    return inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do",
+            name: "action", 
+            choices: [
+                "View employees",
+                "View roles",
+                "View departments",
+                "Add department",
+                "Add role",
+                "Add employee",
+                "Edit employee",
+                "Remove employee",
+                "Exit"
+            ]
+        }
+    ])
+}
+
+
+
+
+// Default response for any other request (Not found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
