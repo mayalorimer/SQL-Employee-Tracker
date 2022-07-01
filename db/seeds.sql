4280a68315f818300669f788da5e265621a0f9ce
Maya Lorimer <maya.lorimer@yahoo.com>
2022-07-01 16:08:32 -0700

    Seeds created

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+    ("Finance"),
+    ("Engineering"),
+    ("HR");
+
+INSERT INTO role (title, salary, department_id) 
+VALUES ("Software Engineer", 120000, 3),
+    ("Accountant", 130000, 2),
+    ("HR Person", 100000, 4),
+    ("Sales Lead", 120000, 1),
+    ("Hardware Engineer", 130000, 3),
+    ("Salesperson", 110000, 1);
+
+
+INSERT INTO employee (first_name, last_name, role, manager_id) 
+VALUES ("John", "Doe", 4, null),
+    ("Greg", "Smith", 1, 1),
+    ("Harrison", "Pint", 2, null);
